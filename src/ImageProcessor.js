714f1e25a9528c295fc811611d5fbff2998ec3ae
DrIronfist <reetsinha@Reets-MBP.attlocal.net>
2023-05-01 07:05:46 -0500

    lol

--- src/ImageProcessor.js
+++ src/ImageProcessor.js
@@ -1,57 +1,9 @@
 import React, { useState } from 'react';
 import perlin2D from './Perlin';
 
-const permutation = [151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 
-    103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247, 120, 234, 75, 0, 
-    26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237, 149, 56, 
-    87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 
-    77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 
-    46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 
-    187, 208, 89, 18, 169, 200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 
-    198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123, 5, 202, 38, 147, 118, 126, 
-    255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42, 223, 183, 
-    170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 
-    172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 
-    104, 218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 
-    241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31, 181, 199, 106, 
-    157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 
-    93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180];
-
-
-
-function perlin2(x, y, permutation) {
-  const p = new Uint8Array(256);
-  for (let i = 0; i < 256; i++) {
-    p[i] = permutation[i % permutation.length];
-  }
-  const X = Math.floor(x) & 255;
-  const Y = Math.floor(y) & 255;
-  x -= Math.floor(x);
-  y -= Math.floor(y);
-  const fade = t => t * t * t * (t * (t * 6 - 15) + 10);
-  const u = fade(x);
-  const v = fade(y);
-  const a = p[X] + Y;
-  const aa = p[a];
-  const ab = p[a + 1];
-  const b = p[X + 1] + Y;
-  const ba = p[b];
-  const bb = p[b + 1];
-  const lerp = (t, a, b) => a + t * (b - a);
-  return lerp(
-    v,
-    lerp(u, grad(p[aa], x, y), grad(p[ba], x - 1, y)),
-    lerp(u, grad(p[ab], x, y - 1), grad(p[bb], x - 1, y - 1))
-  );
-}
-
-function grad(hash, x, y) {
-  const h = hash & 3;
-  return h === 0 ? x + y : h === 1 ? -x + y : h === 2 ? x - y : -x - y;
-}
-
 export function ImageProcessor() {
-    function applyPerlinNoise(imageData, permutation) {
+    function applyPerlinNoise(imageData) {
+        console.log("called");
         const width = imageData.width;
         const height = imageData.height;
         const data = imageData.data;
@@ -59,9 +11,10 @@ export function ImageProcessor() {
           for (let x = 0; x < width; x++) {
             const noise = perlin2D(x,y,octaves,frequency,amplitude,persistence);
             const offset = (y * width + x) * 4;
-            data[offset] += noise * 20;
-            data[offset + 1] += noise * 20;
-            data[offset + 2] += noise * 20;
+            data[offset] += noise * scale;
+            data[offset + 1] += noise * scale;
+            data[offset + 2] += noise * scale;
+            data[offset + 3] += noise * scale;
           }
         }
         return imageData;
@@ -70,6 +23,7 @@ export function ImageProcessor() {
     const [frequency, setFrequency] = useState(0.02);
     const [amplitude, setAmplitude] = useState(1);
     const [persistence, setPersistence] = useState(0.5);
+    const [scale, setScale] = useState(10);
     const [res, setRes] = useState(null);
     
   const [file, setFile] = useState(null);
@@ -89,7 +43,7 @@ export function ImageProcessor() {
         context.drawImage(img, 0, 0);
         var newImageData = context.getImageData(0, 0, img.width, img.height);
         console.log(newImageData)
-        newImageData = applyPerlinNoise(newImageData, permutation);
+        newImageData = applyPerlinNoise(newImageData);
         console.log(newImageData)
         setImageData(newImageData);
       };
@@ -114,7 +68,7 @@ export function ImageProcessor() {
         context.drawImage(img, 0, 0);
         var newImageData = context.getImageData(0, 0, img.width, img.height);
         console.log(newImageData)
-        newImageData = applyPerlinNoise(newImageData, permutation);
+        newImageData = applyPerlinNoise(newImageData);
         console.log(newImageData)
         setImageData(newImageData);
       };
@@ -145,6 +99,10 @@ export function ImageProcessor() {
             <div>
                 <label htmlFor="persistence">Persistence:</label>
                 <input type="number" id="persistence" value={persistence} onChange={e => setPersistence(e.target.value)} min={0.1} max={1} step={0.1} />
+            </div>
+            <div>
+                <label htmlFor="scale">Scale:</label>
+                <input type="number" id="scale" value={scale} onChange={e => setScale(e.target.value)} min={1} max={100} step={5} />
             </div>
             <button type="submit">Generate</button>
         </form>
